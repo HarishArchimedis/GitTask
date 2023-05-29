279b4ac9b383eea03cad5a8f34f48f1be7621711
HarishArchimedis <info.harish.p@gmail.com>
2023-05-29 21:03:11 +0530

    ghg

--- Odd_Even.py
+++ Odd_Even.py
@@ -1,7 +1,7 @@
 num = int(input("Enter your number: "))
 if num % 2 == 0:
-    print(num, "is even")
+    print(num, "even")
 elif num % 2 == 1:
-    print(num, "is odd")
+    print(num, "odd")
 else :
     print("It is not interger")    
